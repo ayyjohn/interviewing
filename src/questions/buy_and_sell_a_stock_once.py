fd893efb0cef6c9ce9519ac63f7d00fc0a7d2b35
Alec Johnson <alecjohnson55@gmail.com>
2021-11-29 20:13:14 -0800

    remove print from buy and sell a stock

--- src/questions/buy_and_sell_a_stock_once.py
+++ src/questions/buy_and_sell_a_stock_once.py
@@ -9,7 +9,6 @@ def buy_and_sell_a_stock_once(prices: list[float]) -> float:
     min_price = prices[0]
 
     for price in prices:
-        print(price, min_price, max_profit)
         min_price = min(min_price, price)
         max_profit = max(max_profit, price - min_price)
     return max_profit
